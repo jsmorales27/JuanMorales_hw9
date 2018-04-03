e5e9deb670a4dbf1782ec2d4a54ebeffee0fe360
jsmorales27 <37352283+jsmorales27@users.noreply.github.com>
2018-04-03 21:57:50 +0000

    Add files via upload

--- JuanMorales_Grafica.py
+++ JuanMorales_Grafica.py
@@ -3,9 +3,10 @@ import numpy as np
 
 c=np.loadtxt("times_cpp.txt", dtype=float,delimiter=",")
 py=np.loadtxt("times_python.txt", dtype=float,delimiter=",")
-
-plt.scatter(c[1],c[0],label="C++")
-plt.scatter(py[1],py[0],label="Python")
+print c
+print py
+plt.scatter(c[:,0],c[:,1],label="C++",color="red")
+plt.scatter(py[:,0],py[:,1],label="Python")
 plt.legend()
 plt.savefig("cpp_vs_python.png")
 
